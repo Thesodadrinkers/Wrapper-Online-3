c6a83678a123e18d78eabaa4a097d43dc0d0e480
2Epik4u <justinabossmlg@gmail.com>
2020-08-11 14:35:13 -0400

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+echo = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+echo Starting Wrapper Offfline Node.js webserver
+echo = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+cd $HOME/desktop/wrapper-offline/wrapper
+npm start 
