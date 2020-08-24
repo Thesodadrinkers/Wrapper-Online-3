00d11550e649e2b0323fa91a65de6b576a83d9cd
2Epik4u <justinabossmlg@gmail.com>
2020-08-24 13:37:40 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,5 +2,4 @@
 echo = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 echo Starting Wrapper Offfline Node.js webserver
 echo = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
-cd $HOME/desktop/wrapper-offline/wrapper
 npm start 
