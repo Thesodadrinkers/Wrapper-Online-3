ebb860ef14a472f63c02f9cd8c65f4eb70186cd9
2Epik4u <justinabossmlg@gmail.com>
2020-09-08 20:50:58 -0400

    Update load.js

--- movie/load.js
+++ movie/load.js
@@ -29,6 +29,7 @@ module.exports = function (req, res, url) {
 			movie.loadZip(url.query.movieId).then(b =>
 				res.end(Buffer.concat([base, b]))
 			).catch(e => res.end('1'));
+			});
 			return true;
 		}
 		default: return;
