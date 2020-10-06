4df8362c0282541355c1f369b494db8d2bba5935
2Epik4u <justinabossmlg@gmail.com>
2020-10-06 13:05:12 -0400

    bug fix

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-require('./server');
+const sessions = require('./data/sessions');
+const start = '/events/';
+module.exports = function (req, res, url) {
+	if (!url.path.startsWith(start)) return;
+	switch (url.path.substr(start.length)) {
+		case 'close': {
+			sessions.remove(req);
+			break;
+		}
+		default: {
+			res.end();
+			return false;
+		};
+	}
+	res.end();
+	return true;
+}
